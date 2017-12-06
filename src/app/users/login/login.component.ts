8a4a0160e974b2c7805b8dfbc0d4da7b7591a768
Marco <marcohavermans+github@hotmail.nl>
2017-12-06 14:58:49 +0100

    Added login card with form.

--- src/app/users/login/login.component.ts
+++ src/app/users/login/login.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BaseComponent } from 'app/shared/basecomponent.class';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: 'login.component.html'
 })
-export class LoginComponent extends BaseComponent {
+export class LoginComponent extends BaseComponent implements OnInit {
+  /**
+   * The login form
+   */
+  public loginForm: FormGroup;
+
+  /**
+   * Determines if a submit is in progress
+   */
+  public submitInProgress: boolean;
+
   constructor() {
     super()
+  }
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  /**
+   * Handles the onsubmit of the form
+   */
+  public onSubmit() {
+    this.submitInProgress = true;
+    debugger;
+  }
+
+  /**
+   * Initializes the form
+   */
+  private initForm() {
+    this.loginForm = new FormGroup({
+      'email': new FormControl('', [Validators.email, Validators.required]),
+      'password': new FormControl('', Validators.required)
+    });
   }
 }
--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 
 import { LayoutModule } from '../shared/layout/layout.module';
 import { CommonModule } from '@angular/common';
-import { LoginComponent } from 'app/users/login/login.component';
-import { UsersRoutingModule } from 'app/users/users-routing.module';
+import { LoginComponent } from './login/login.component';
+import { UsersRoutingModule } from './users-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,9 @@ import { UsersRoutingModule } from 'app/users/users-routing.module';
   imports: [
     CommonModule,
     LayoutModule,
-    UsersRoutingModule
+    UsersRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: []
 })
