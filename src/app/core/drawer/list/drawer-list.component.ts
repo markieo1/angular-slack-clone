3adc698a873563c19032fa8f5383778bc8b8a9b5
Marco <marcohavermans+github@hotmail.nl>
2017-12-06 18:24:00 +0100

    Removed unneeded menu items.

--- src/app/core/drawer/list/drawer-list.component.ts
+++ src/app/core/drawer/list/drawer-list.component.ts
@@ -6,44 +6,23 @@ import { Component } from '@angular/core';
 })
 export class DrawerListComponent {
   navLinks = [
-    { name: 'Buttons', route: 'buttons', icon: 'adjust', desc: '' },
     {
-      name: 'Card', route: 'card-demo', icon: 'credit_card',
-      desc: 'A card is a sheet of material that serves as an entry point to more detailed information.'
+      name: 'Dashboard',
+      route: '/',
+      icon: 'dashboard',
+      desc: 'An overview of what you can do in this application'
     },
     {
-      name: 'Dialog', route: 'dialog-demo', icon: 'question_answer',
-      desc: 'Dialogs inform users about a specific task, require decisions, or involve multiple tasks.'
+      name: 'Groups',
+      route: '/groups',
+      icon: 'group',
+      desc: 'An overview of all the groups you can join and chat in'
     },
     {
-      name: 'Drawer', route: 'drawer-demo', icon: 'code',
-      desc: 'Drawers slide in from the left and contains the navigation destinations for your app.'
-    },
-    { name: 'Icon', route: 'icon-demo', icon: 'star', desc: 'Icon supports font ligatures and font icons using CSS.' },
-    { name: 'Inputs and Controls', route: 'inputs-controls', icon: 'input', desc: '' },
-    {
-      name: 'Linear-Progress', route: 'linear-progress-demo', icon: 'compare_arrows',
-      desc: 'Progress and activity indicators are visual indications of an app loading content.'
-    },
-    {
-      name: 'List', route: 'list-demo', icon: 'list',
-      desc: 'Lists present multiple line items vertically as a single continuous element.'
-    },
-    {
-      name: 'Menu', route: 'menu-demo', icon: 'menu',
-      desc: 'Menus display a list of choices on a transient sheet of material.'
-    },
-    {
-      name: 'Snackbar', route: 'snackbar-demo', icon: 'info_outline',
-      desc: 'Snackbars provide brief feedback through a message at the bottom of the screen.'
-    },
-    {
-      name: 'Tabs', route: 'tab-demo', icon: 'tab',
-      desc: 'Tabs make it easy to explore and switch between different views.'
-    },
-    {
-      name: 'Toolbar', route: 'toolbar-demo', icon: 'remove',
-      desc: 'Toolbars appear a step above the sheet of material affected by their actions.'
-    },
+      name: 'Users',
+      route: '/users',
+      icon: 'person',
+      desc: 'An overview of all the users in this application'
+    }
   ];
 }
