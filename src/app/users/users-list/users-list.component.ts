1f1881c677f96e9d4830071f954ae0e45e6f0489
Marco <marcohavermans+github@hotmail.nl>
2017-12-07 09:13:12 +0100

    Removed menu

--- src/app/users/users-list/users-list.component.ts
+++ src/app/users/users-list/users-list.component.ts
@@ -1,18 +1,9 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { BaseComponent } from '../../shared/basecomponent.class';
-import { MdcMenu } from '@angular-mdc/web';
 
 @Component({
   selector: 'app-users-list',
   templateUrl: 'users-list.component.html'
 })
 export class UsersListComponent extends BaseComponent {
-
-  @ViewChild('menu')
-  menu: MdcMenu;
-
-
-  public openMenu() {
-    this.menu.open();
-  }
 }
