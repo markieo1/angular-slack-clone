23cbbc6e7e583ab21668e1fcbaf71155a0fef29a
Marco <marcohavermans+github@hotmail.nl>
2017-12-07 18:08:14 +0100

    Fixed list item clickable. Also added time to list items

--- src/app/groups/groups-list/groups-list.component.ts
+++ src/app/groups/groups-list/groups-list.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, ViewChild, NgZone } from '@angular/core';
 import { BaseComponent } from '../../shared/basecomponent.class';
 import { GroupService } from '../group.service';
 import { Group } from '../group.model';
+import { MdcList } from '@angular-mdc/web';
 
 @Component({
   selector: 'app-groups-list',
@@ -14,7 +15,10 @@ export class GroupsListComponent extends BaseComponent implements OnInit {
    */
   public groups: Group[];
 
-  constructor(private groupService: GroupService) {
+  @ViewChild(MdcList)
+  public mdcList: MdcList;
+
+  constructor(private groupService: GroupService, private cdr: ChangeDetectorRef, private zone: NgZone) {
     super();
     this.groups = [];
   }
@@ -22,7 +26,12 @@ export class GroupsListComponent extends BaseComponent implements OnInit {
   ngOnInit(): void {
     this.groupService.getGroups().subscribe((groups) => {
       this.groups = groups;
+
+      if (this.mdcList) {
+        setTimeout(() => {
+          this.mdcList.ngAfterContentInit();
+        }, 100);
+      }
     });
   }
-
 }
