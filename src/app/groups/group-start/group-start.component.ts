af8331845f523fb808cdd4ee3a01967260281647
Marco <marcohavermans+github@hotmail.nl>
2017-12-08 18:46:55 +0100

    Removed template file for groupstart

--- src/app/groups/group-start/group-start.component.ts
+++ src/app/groups/group-start/group-start.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-group-start',
-  templateUrl: './group-start.component.html'
+  template: ''
 })
 export class GroupStartComponent {
 }
