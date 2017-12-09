e5a62f838bca13339d8ed707f8a1aefc6fa93b81
Marco <marcohavermans+github@hotmail.nl>
2017-12-09 16:17:32 +0100

    Added chat.model

---
+++ src/app/chats/chat.model.ts
@@ -0,0 +1,38 @@
+import { BaseModel } from '../shared/base/basemodel.class';
+
+export class ChatMessage extends BaseModel {
+  /**
+   * The message that is sent
+   */
+  message: string;
+
+  /**
+   * The sent date
+   */
+  sentAt: Date;
+
+  /**
+   * The last edited date
+   */
+  lastEdit: Date;
+
+  /**
+   * From whoem the message is
+   */
+  from: {
+    /**
+     * The nickname of the user
+     */
+    nickname: string;
+
+    /**
+     * The id of the user
+     */
+    id: string;
+  };
+
+  /**
+   * The group id for this message
+   */
+  groupId: string;
+}
