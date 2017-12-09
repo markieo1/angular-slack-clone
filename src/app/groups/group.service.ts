d1c3c1c02698d83cbce34b7b376470f1a7296a06
Marco <marcohavermans+github@hotmail.nl>
2017-12-09 16:38:13 +0100

    Removed unneeded imports

--- src/app/groups/group.service.ts
+++ src/app/groups/group.service.ts
@@ -1,10 +1,6 @@
-import { environment } from '../../environments/environment';
-import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
 import { AuthHttp } from 'angular2-jwt';
 import { Group } from './group.model';
-import { RequestOptionsArgs, Headers } from '@angular/http';
-import { ReplaySubject } from 'rxjs/ReplaySubject';
 import { BaseCrudService } from '../shared/base/basecrudservice.class';
 
 @Injectable()
--- src/app/users/user.service.ts
+++ src/app/users/user.service.ts
@@ -3,8 +3,6 @@ import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
 import { AuthHttp } from 'angular2-jwt';
 import { User } from '../shared/user.model';
-import { RequestOptionsArgs, Headers } from '@angular/http';
-import { ReplaySubject } from 'rxjs/ReplaySubject';
 import { BaseCrudService } from '../shared/base/basecrudservice.class';
 
 @Injectable()
